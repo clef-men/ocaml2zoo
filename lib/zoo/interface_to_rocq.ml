2aaa00a07fb64c17108388d20ecfb1179e261c0a
Clément <clef-men@orange.fr>
2026-04-04 16:51:41 +0200

    lib/zoo/interface_to_rocq: Newline after imports.

--- lib/zoo/interface_to_rocq.ml
+++ lib/zoo/interface_to_rocq.ml
@@ -1,6 +1,7 @@
 let transl (t : Interface.t) =
   List.concat
   [ [ Rocq.require RequireImport t.library [t.module_ ^ "__code"]
+    ; Rocq.newline
     ]
   ; List.map (fun global ->
       Rocq.opaque LocalityGlobal (Fmt.str "%s_%s" t.module_ global)
