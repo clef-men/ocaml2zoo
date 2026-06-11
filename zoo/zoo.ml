b6d6ae12b8b68017b4fc23a8e0a48a0fa2bcafd2
Clément <clef-men@orange.fr>
2026-06-11 16:55:49 +0200

    zoo/zoo: Relax type restriction on prophecy resolution backing expressions.

--- zoo/zoo.ml
+++ zoo/zoo.ml
@@ -4,23 +4,21 @@ type dummy =
 let dummy =
   Obj.repr (ref ())
 
-type (_, _) proph =
+type _ proph =
   dummy
-type 'a proph' =
-  (unit, 'a) proph
 
-let proph () =
+let[@inline] proph () =
   dummy
-let resolve_with x _ _ =
+let[@inline] resolve_with x _ _ =
   x
-let resolve_silent proph v_proph =
+let[@inline] resolve_silent proph v_proph =
   resolve_with ((fun () -> ()) ()) proph v_proph
-let resolve proph v_proph =
+let[@inline] resolve proph v_proph =
   resolve_silent proph v_proph ;
   v_proph
 
 type id =
   dummy
 
-let id () =
+let[@inline] id () =
   dummy
